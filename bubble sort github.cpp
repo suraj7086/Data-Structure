69dfb29dd3eebba9309fb013e391570b7144411b
Suraj Singh <55402839+suraj7086@users.noreply.github.com>
2019-10-14 19:46:52 +0530

    Update bubble sort github.cpp

--- bubble sort github.cpp
+++ bubble sort github.cpp
@@ -1,49 +1,39 @@
+
 #include<iostream>
 using namespace std;
 int main()
-{   /*declare an array of size 10*/
-    int arr[10];
-    cout<<"Enter the elemens "<<endl;
-    /*taking input from user*/
-    for(int i=0;i<10;i++)
+{   cout<<"This is the implementation of bubble sort"<<endl;
+    cout<<"Enter the size of the array"<<endl;
+    int size;
+    cin>>size;
+    cout<<"Enter the element of the array"<<endl;
+    int arr[size];
+    for(int i=0;i<size;i++)
     {
         cin>>arr[i];
     }
-   /*printing number entered by the user*/
-    for(int i=0;i<10;i++)
+    cout<<"Entered array is :"<<endl;
+    for(int i=0;i<size;i++)
     {
         cout<<arr[i]<<"\t";
     }
     cout<<endl;
-    cout<<"Enter the value to search"<<endl;
-    int num;
-    cin>>num;
-    int f=0;
-    for(int i=0;i<10;i++)
+    cout<<"Sorted list is : "<<endl;
+    for(int i=0;i<=size-1;i++)
     {
-        int beg=0,last=9,mid;
-        mid=(int)(beg+last)/2;
-        if(num==arr[mid])
+        for(int j=0;j<size;j++)
         {
-            f=mid+1;
-            cout<<"number is found at position "<<f<<endl;
-            break;
+            if(arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
         }
-        else if(num<arr[mid])
-        {
-            last=mid-1;
-        }
-        else
-        {
-            beg=mid+1;
-        }
-
-
     }
-    /*we have initialised the f=0 in upper part here f==0 "is because if f will not change means element is not found*/
-    if(f==0)
+    cout<<endl;
+    for(int i=0;i<size;i++)
     {
-        cout<<"element is not found"<<endl;
+        cout<<arr[i]<<"\t";
     }
-    return 0;
 }
